fc1343032978e9b94b9ef3ff69dca07ddf07d82d
ash_riple <ash_riple@cf6ff63e-2f66-4ffb-8f1d-8b819dcf5cb1>
2013-01-24 02:42:42 +0000

    Made different scripts for top-level simulation to run under Linux.

--- sim/top/sim.bat
+++
@@ -1,7 +0,0 @@
-title %CD%
-
-SET LM_LICENSE_FILE=D:\Programs\license_win.dat
-SET MODEL_TECH=D:\Programs\modeltech_6.5g\win32
-SET PATH=D:\Programs\modeltech_6.5g\win32;D:\Programs\modeltech_6.5g\gcc-4.2.1-mingw32\bin
-
-vsim -do sim.do
