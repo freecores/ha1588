cd7656ae9320a38a95db98d5fcde865b145e6674
ash_riple <ash_riple@cf6ff63e-2f66-4ffb-8f1d-8b819dcf5cb1>
2013-01-24 02:40:01 +0000

    Made different scripts for top-level simulation to run under Windows.

--- sim/top/sim_win.bat
+++ sim/top/sim_win.bat
@@ -4,4 +4,4 @@ SET LM_LICENSE_FILE=D:\Programs\license_win.dat
 SET MODEL_TECH=D:\Programs\modeltech_6.5g\win32
 SET PATH=D:\Programs\modeltech_6.5g\win32;D:\Programs\modeltech_6.5g\gcc-4.2.1-mingw32\bin
 
-vsim -do sim.do
+vsim -do sim_win.do
